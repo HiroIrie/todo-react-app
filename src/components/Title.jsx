741fcf0ad90d29d5abcec4e1bdb3334dd349ca38
HiroIrie <h0514c0325@gmail.com>
2023-02-02 15:02:04 +0900

    first commit

---
+++ src/components/Title.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Title = () => {
+    return (
+        <div>
+            <header>
+                <h1>やる事リスト</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Title
