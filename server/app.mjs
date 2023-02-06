e9efdb6f974237b543b0bbd708cbcc5802b00283
HiroIrie <h0514c0325@gmail.com>
2023-02-06 11:36:39 +0900

    アップロードOKです

--- server/app.mjs
+++ server/app.mjs
@@ -9,11 +9,11 @@ const app=express();
 const port=process.env.PORT||3001;
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('build'));
 app.use('/api',apiRouter);
 
 app.get('*',(req,res)=>{
- const innerHtml=path.resolve('public','index.html');
+ const innerHtml=path.resolve('build','index.html');
  res.sendFile(innerHtml);
 })
 
